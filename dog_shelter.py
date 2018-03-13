0539943b6122fff7d54fd198264038a07476b2ff
Masilionytė Dalia <dmasilionyte@gmail.com>
2018-03-13 14:35:35 +0200

    added comments and fixed a typo

--- dog_shelter.py
+++ dog_shelter.py
@@ -6,7 +6,6 @@ import os
 
 app = Flask(__name__)
 
-
 dogs_db = [
 	{'id' : '1', 'breed' : 'French bulldog', 'name' : 'Doggo', 'temporary guardian' : 'NONE'},
 	{'id' : '2', 'breed' : 'Chow Chow', 'name' : 'Sir Pup', 'temporary guardian' : 'NONE'},
@@ -15,17 +14,16 @@ dogs_db = [
 	{'id' : '5', 'breed' : 'German Shepherd', 'name' : 'Rex', 'temporary guardian' : 'NONE'}
 ]
 
-
 @app.route('/')
 def hello():
 	return'Welcome to the puppy shelter'
 
-
+# GET information about all dogs from database as JSON
 @app.route('/dogs', methods=['GET'])
 def get_all_dogs():
 	return jsonify({'Our Pupps':dogs_db})
 
-
+# DELETE a dog from a database by ID (adopt)
 @app.route('/dogs/<dog_id>', methods=['DELETE'])
 def adopt_dog(dog_id):
 	adopted_dog = [ dog for dog in dogs_db if (dog['id'] == dog_id)]
@@ -34,7 +32,8 @@ def adopt_dog(dog_id):
 	dogs_db.remove(adopted_dog[0])
 	return jsonify({'Your adopted pup:':adopted_dog[0]})
 
-
+# POST a dog to a database (give away)
+# Name is in url, id and breed have to be provided as JSON
 @app.route('/dogs/<give_away_dog_name>', methods=['POST'])
 def give_away_dog(give_away_dog_name):
 	new_dog = {
@@ -46,11 +45,13 @@ def give_away_dog(give_away_dog_name):
 	dogs_db.append(new_dog)
 	return jsonify({'That was very cruel':new_dog})
 
+# PUT (become) a dog guardian
+# ID in URL, guardian name - provided as JSON
 @app.route('/dogs/<dog_id>', methods = ['PUT'])
 def become_guardian(dog_id):
 	guarded_dog = [ dog for dog in dogs_db if (dog['id'] == dog_id)]
 	guarded_dog[0]['temporary guardian'] = request.json['temporary guardian']
-	return jsonify({'You became a guardian'}: guarded_dog[0])
+	return jsonify({'You became a guardian': guarded_dog[0]})
 
 
 if __name__ == "__main__":
